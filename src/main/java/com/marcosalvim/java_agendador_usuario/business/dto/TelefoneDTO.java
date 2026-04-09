476fe660bfaccd84f7aee139ba2769be102e5f64
Marcos Alvim Faria <marcosalvimfaria@Marcoss-MacBook-Air.local>
2026-04-09 09:27:51 -0300

    chore: create user

---
+++ src/main/java/com/marcosalvim/java_agendador_usuario/business/dto/TelefoneDTO.java
@@ -0,0 +1,15 @@
+package com.marcosalvim.java_agendador_usuario.business.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TelefoneDTO {
+
+    private Long id;
+    private String numero;
+    private String ddd;
+}
