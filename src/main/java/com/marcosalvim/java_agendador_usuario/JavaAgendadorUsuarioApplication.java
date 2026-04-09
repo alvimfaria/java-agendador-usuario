e9fba10fe22ba2385f7e37eced99ffd236ee6e98
Marcos Alvim Faria <marcosalvimfaria@Marcoss-MacBook-Air.local>
2026-04-09 08:14:13 -0300

    start project

---
+++ src/main/java/com/marcosalvim/java_agendador_usuario/JavaAgendadorUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.marcosalvim.java_agendador_usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAgendadorUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAgendadorUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcosalvim/java_agendador_usuario/JavaAgendadorUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosalvim.java_agendador_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAgendadorUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
